cb26575d663c13a79158241d532612997f795f89
lohiavardhan <lohiavardhan@gmail.com>
2026-03-29 20:45:11 +0800

    added the logic to get all emails for the past hour

--- dags/expense_tracker.py
+++ dags/expense_tracker.py
@@ -1,6 +1,14 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 default_args = {
     'owner': 'vardhan',
@@ -8,10 +16,34 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+def get_gmail_service():
+    creds = None
+    # token.json stores the user's access/refresh tokens after first auth
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    # If no valid creds, do the OAuth flow
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=8080)
+
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return build('gmail', 'v1', credentials=creds)
+
 def fetch_emails():
-    """Pull bank emails from Gmail API"""
-    # TODO: Gmail API logic
+    """Pull all emails from Gmail API"""
+
+    service = get_gmail_service()
+    results = service.users().messages().list(userId='me', q='newer_than:1h').execute()
+    messages = results.get('messages', [])
+    
     print("Fetching emails...")
+    
 
 def parse_transactions(**context):
     """Filter bank emails and extract amount, merchant, date"""
